2a0b748e9c1c7465bb8bfc3d9e2ae07c5fbfaf78
Emmanuel Adiba <eabaagah@gmail.com>
2020-11-05 01:07:45 +0000

    search weather by city

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,21 +1,18 @@
 window.addEventListener("load", () => {
-    let long // longitude
-    let lat // latitude
     
     let temperatureDegree = document.querySelector('.temperature-degree');
     let temperatureDescription = document.querySelector('.temperature-description');
     let locationTimezone = document.querySelector('.location-timezone');
     let country = document.querySelector('.location-timezone span');
     let temperatureSection = document.querySelector('.degree-section');
-    let temperatureSpan = document.querySelector('.degree-section span');
+    let temperatureSpan = document.querySelector('.degree-section span'); 
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+    document.querySelector('button').addEventListener('click', function (){
+        
+        let city = document.querySelector('.getCity').value;
+        const proxy = "https:cors-anywhere.herokuapp.com/";
+        const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=21fcbd5f02de0ad45dddb98db8e31b71`;
 
-            const proxy = "https:cors-anywhere.herokuapp.com/";
-            const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=21fcbd5f02de0ad45dddb98db8e31b71`;
 
             fetch(api)
                 .then(res => {
@@ -57,8 +54,5 @@ window.addEventListener("load", () => {
             });
 
         });
-    } else {
-        document.querySelector('location-timezone').innerHTML = "Please enable geolocation";
-    }
 
-});
+    });
